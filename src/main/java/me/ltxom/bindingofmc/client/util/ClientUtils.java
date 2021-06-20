608a99bb2c451c42d61626db525e32b5aad8a5bd
Tom Liu <ltxom@uw.edu>
2021-06-20 17:04:53 +0800

    Remove ClientUtils.java

--- src/main/java/me/ltxom/bindingofmc/client/util/ClientUtils.java
+++
@@ -1,8 +0,0 @@
-package me.ltxom.bindingofmc.client.util;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-
-@OnlyIn(Dist.CLIENT)
-public class ClientUtils {
-}
